ffa51ae9eded1a8975c25f48b5700157cd642a02
emmanpestan <emmanuel.empestan@gmail.com>
2026-04-30 00:34:56 +0800

    process integers into even squares and odd cubes

--- square_even_cube/square_even_cube.py
+++ square_even_cube/square_even_cube.py
@@ -11,3 +11,19 @@ class IntegerProcessor:
     def create_double_and_triple_files(self):
         even_squares = []
         odd_cubes = []
+
+        with open(self.input_file, "r") as file:
+            numbers = file.readlines()
+
+        for number in numbers:
+            number = number.strip()
+
+            if number == "":
+                continue
+
+            number = int(number)
+
+            if number % 2 == 0:
+                even_squares.append(str(number ** 2))
+            else:
+                odd_cubes.append(str(number ** 3))
